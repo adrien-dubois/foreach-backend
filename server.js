9aae4fcf300f81d994e4c3df81190c15024abd66
adrien-dubois <mcsnoos@gmail.com>
2022-04-17 15:46:53 +0200

    [FIX]User schema for notifications

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
-const User = require('./models/User')
-const Message = require('./models/Message')
+
+const User = require('./models/User');
+const Message = require('./models/Message');
+
 const rooms = [
     'General',
     'Tech',
     'Crypto',
-    'Gaming'    
+    'Gaming'
 ];
+const cors = require('cors');
 
 
 app.use(express.urlencoded({extended: true}));
@@ -29,6 +31,11 @@ const io = require('socket.io')(server, {
 })
 
 
+app.get('/rooms', (req, res)=> {
+    res.json(rooms)
+})
+
+
 async function getLastMessagesFromRoom(room){
     let roomMessages = await Message.aggregate([
         {$match: {to: room}},
@@ -45,7 +52,7 @@ function sortRoomMessagesByDate(messages){
         date1 = date1[2] + date1[0] + date1[1];
         date2 = date2[2] + date2[0] + date2[1];
 
-        return date1 < date2 ? '-1' : 1
+        return date1 < date2 ? -1 : 1
     })
 }
 
@@ -56,7 +63,7 @@ io.on('connection', (socket)=> {
 
     socket.on('new-user', async ()=>{
         const members = await User.find();
-        io.emit('new-user', members)
+        io.emit('new-user', members);
     })
 
     socket.on('join-room', async(room)=> {
@@ -67,16 +74,15 @@ io.on('connection', (socket)=> {
     })
 
     socket.on('message-room', async(room, content, sender, time, date) => {
-        console.log('Nouveau message : ', content);
         const newMessage = await Message.create({content, from: sender, time, date, to: room});
         let roomMessages = await getLastMessagesFromRoom(room);
         roomMessages = sortRoomMessagesByDate(roomMessages);
-        
+
         // sending message to room
         io.to(room).emit('room-messages', roomMessages);
 
+        // And send notification when sending new message
         socket.broadcast.emit('notifications', room)
-
     })
 
     app.delete('/logout', async(req, res)=>{
@@ -96,11 +102,6 @@ io.on('connection', (socket)=> {
     })
 
 })
-
-app.get('/rooms', (req, res) => {
-    res.json(rooms)
-})
-
 
 server.listen(PORT, () => {
     console.log('listening to port ', PORT);
